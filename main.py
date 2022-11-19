12ed159897a8ffdd7bdc332891785ccb5040fc86
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-11-19 00:01:49 -0500

    hola

--- main.py
+++ main.py
@@ -175,97 +175,7 @@ def eliminarMesas(id):
     response =requests.delete(url, headers=headers)
     return jsonify(response.status_code==204)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #################################RESULTADOS#########################################
-
 
 @app.route("/resultados",methods=['GET'])
 def getResultados():
@@ -285,26 +195,6 @@ def crearResultados():
     print(json)
     return jsonify(json)
 
-
-
-@app.route("/", methods=['GET'])
-def test():
-    json = {}
-    json["message"] = "Server running ..."
-    return jsonify(json)
-
-
-def loadFileConfig():
-    with open('config.json') as f:
-        data = json.load(f)
-    return data
-
-
-if __name__ == '__main__':
-    dataConfig = loadFileConfig()
-    print("Server running : "+"http://"+dataConfig["url-backend"]+":" +
-          str(dataConfig["port"]))
-    serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
 
 #################################PARTIDOS#########################################
 
